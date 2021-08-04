09b92be20c70e0e18d8b41b7471c772f5e08916b
David Fiaty <dfiaty@gmail.com>
2021-08-04 15:21:10 +0100

    Test update

--- wallet.js
+++ wallet.js
@@ -1,37 +1,24 @@
 (function() {
+    // Defaults
+    var defaults = {
+        param1: 600,
+        param2: 280,
+        param3: true
+    }
+
     // Constructor
     this.Wallet = function() {
         this.param1 = 'test 1';
         this.param2 = 'test 2';
         this.param3 = 'test 3';
-    }
 
-    // Defaults
-    var defaults = {
-        maxWidth: 600,
-        minWidth: 280,
-        overlay: true
-    }
-
-    // Add options 
-    if (arguments[0] && typeof arguments[0] === "object") {
-        this.options = extendDefaults(defaults, arguments[0]);
-    }
-
-    // Extend defaults
-    function extendDefaults(source, properties) {
-        var property;
-        for (property in properties) {
-            if (properties.hasOwnProperty(property)) {
-                source[property] = properties[property];
-            }
+        // Initialize
+        this.init = function () {
+            console.log(this);
         }
-        return source;
-    }
 
-    // Initialize
-    function wallet() {
-        console.log(this.options);
+        this.init();
+
     }
 
 } ());
