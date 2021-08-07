44c7dc1cd7295eedb2c7fe341c79a0e32052a003
David Fiaty <dfiaty@gmail.com>
2021-08-07 18:42:22 +0100

    Build

--- src/js/psp/googlepay.js
+++ src/js/psp/googlepay.js
@@ -14,9 +14,8 @@ module.exports = class GooglePay {
         var textnode = document.createTextNode("Water")
         document.getElementById('wallet').appendChild(textnode);
 
-
+        // Initialize Google Pay
         script(process.env.GOOGLE_PAY_SCRIPT, function() {
-            // Initialize Google Pay
             console.log('GooglePay');
             console.log(self.config);
         });
